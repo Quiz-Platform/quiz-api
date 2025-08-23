0871a7a96c1f436ddd6ddd167d8a3d178b2e1c8c
Constantine Rafikov <2constantine@protonmail.com>
2025-08-23 19:01:04 -0300

    Add Firebase

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import questionsAndAnswers from './loaders/questions-and-answers';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
 
 dotenv.config();
+const firebaseApp = initializeApp(firebaseConfig);
 const app = express();
+const analytics = getAnalytics(firebaseApp);
 const port = process.env.PORT ?? 8080;
 const cors = require('cors');
 
