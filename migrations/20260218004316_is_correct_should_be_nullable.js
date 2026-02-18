c6e0a8e4000ad30cdea50d2774ff7e0b8f2941e2
Tina Rafikova <tinarafikova@proton.me>
2026-02-17 22:02:13 -0300

    fix: fix non-nullable is_correct column for a new workflow

---
+++ migrations/20260218004316_is_correct_should_be_nullable.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  await knex.schema.alterTable('answers', (table) => {
+    table.boolean('is_correct').nullable().alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema.alterTable('answers', (table) => {
+    table.boolean('is_correct').notNullable().alter();
+  });
+};
